a2284bb0d676e1c0cc4e69679080200c10c817d3
Unknow248 <tonkla.tonkla248@gmail.com>
2020-04-17 13:47:12 +0700

    qwre

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,7 +18,7 @@ public class Main {
         System.out.printf("Number1 * Number2=");
         System.out.println(num1*num2);
         System.out.printf("Number1 / Number2=");
-        System.out.println(num1/num2);
+        System.out.println((float)num1/num2);
 
 
     }
