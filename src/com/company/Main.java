921cc930fe19c6ffd0a1e989715e90e1468ad0fc
Unknow248 <tonkla.tonkla248@gmail.com>
2020-04-17 13:32:43 +0700

    q

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-       Scanner scan = new Scanner(System.in);
-       int num1 = scan.nextInt();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Input number 1 = ");
+        int num1 = scan.nextInt();
+        System.out.printf("Input number 2 = ");
         int num2 = scan.nextInt();
-        System.out.printf("Input number 1 =");
-        System.out.println(num1);
-        System.out.printf("Input number 2 =");
-        System.out.println(num2);
         System.out.printf("Number1 + Number2 =");
         System.out.println(num1+num2);
         System.out.printf("Number1 - Number2 =");
