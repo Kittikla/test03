cf3981ae74393c5d0251eb19ce5b21a58846a47b
Unknow248 <tonkla.tonkla248@gmail.com>
2020-04-17 13:45:29 +0700

    qwre

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,17 +7,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.printf("Input number 1 = ");
+        System.out.printf("Input number 1= ");
         int num1 = scan.nextInt();
-        System.out.printf("Input number 2 = ");
+        System.out.printf("Input number 2= ");
         int num2 = scan.nextInt();
-        System.out.printf("Number1 + Number2 =");
+        System.out.printf("Number1 + Number2=");
         System.out.println(num1+num2);
-        System.out.printf("Number1 - Number2 =");
+        System.out.printf("Number1 - Number2=");
         System.out.println(num1-num2);
-        System.out.printf("Number1 * Number2 =");
+        System.out.printf("Number1 * Number2=");
         System.out.println(num1*num2);
-        System.out.printf("Number1 / Number2 =");
+        System.out.printf("Number1 / Number2=");
         System.out.println(num1/num2);
 
 
